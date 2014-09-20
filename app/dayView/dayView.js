8224a14dc76ca2628755c260d23c22f6c3a47fa3
Franco Cedillo <franco.cedillo@gmail.com>
2014-09-20 16:54:04 -0500

    adds Day detail, no ODs jet

---
+++ app/dayView/dayView.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('myApp.dayView', ['ngRoute', 'pnpServices'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/day/:id', {
+    templateUrl: '/dayView/dayView.html',
+    controller: 'DayCtrl'
+  });
+}])
+
+.controller('DayCtrl', [ '$routeParams', '$scope', 'Day', function($routeParams, $scope, Day) {
+  $scope.day = Day.findById({id: $routeParams.id});
+}]);
---
+++ app/dayView/dayView_test.js
@@ -0,0 +1,17 @@
+'use strict';
+
+describe('myApp.dayView module', function() {
+
+  beforeEach(module('myApp.dayView'));
+
+  describe('Day controller', function(){
+
+    it('should ....', inject(function($controller) {
+      //spec body
+      var scope = {};
+      var dayCtrl = $controller('DayCtrl');
+      expect(dayCtrl).toBeDefined();
+    }));
+
+  });
+});
